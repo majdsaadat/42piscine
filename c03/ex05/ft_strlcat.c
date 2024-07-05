8a747ee02bd0f580e8e169f64533ff2d573be374
majdsaadat <150346255+majdsaadat@users.noreply.github.com>
2024-07-05 20:06:44 +0300

    Create ft_strlcat.c

---
+++ c03/ex05/ft_strlcat.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <stdio.h>
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	unsigned int	i;
+
+	dest_len = 0;
+	src_len = 0;
+	while (dest[dest_len] != '\0')
+		dest_len++;
+	while (src[src_len] != '\0')
+		src_len++;
+	if (size <= dest_len)
+		return (size + src_len);
+	i = 0;
+	while (src[i] != '\0' && (dest_len + i) < (size - 1))
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
+}
+/*
+int	main(void)
+{
+	char dest [20] = "Hello";
+	char src [] = "World";
+	
+	ft_strlcat(dest, src, 0);
+	printf("%d", ft_strlcat(dest, src, 0));
+}*/
