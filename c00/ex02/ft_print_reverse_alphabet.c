c7490bc14554a4f126ab1350eb15e6967c4bd75a
majdsaadat <150346255+majdsaadat@users.noreply.github.com>
2024-07-05 18:59:00 +0300

    Create ft_print_reverse_alphabet.c

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** /
+/ /
+/ ::: :::::::: /
+/ ft_print_reverse_alphabet.c :+: :+: :+: /
+/ +:+ +:+ +:+ /
+/ By: msaadat marvin@42.fr +#+ +:+ +#+ /
+/ +#+#+#+#+#+ +#+ /
+/ Created: 2024/06/05 12:53:49 by msaadat #+# #+# /
+/ Updated: 2024/06/05 12:55:52 by msaadat ### ########.fr /
+/ /
+/ ************************************************************************** */
+#include<unistd.h>
+void ft_print_reverse_alphabet(void)
+{
+char charactor;
+charactor = 'z';
+while (charactor >= 'a' )
+{
+	write (1, &charactor, 1);
+	charactor--;
+}
+}
