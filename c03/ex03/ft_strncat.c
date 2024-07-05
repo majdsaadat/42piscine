97d22b34959bbdbc798fc2244a1dd47714199a41
majdsaadat <150346255+majdsaadat@users.noreply.github.com>
2024-07-05 20:05:07 +0300

    Create ft_strncat.c

---
+++ c03/ex03/ft_strncat.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<unistd.h>
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0' && j < nb)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
+/*
+int	main(void)
+{
+	char dest[50] = "majd";
+	char src[] = "saadat";
+
+	ft_strncat(dest, src, 8);
+	printf("%s", dest);
+	return (0);
+}
+*/
