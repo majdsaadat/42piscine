061377b69fdb9119b8415dc41f7f81135f112ab5
majdsaadat <150346255+majdsaadat@users.noreply.github.com>
2024-07-05 20:32:45 +0300

    Create 	ft_putstr.c

---
+++ c04/ex01/ft_putstr.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+/*
+int	main(void)
+{
+	char c[20]= "majd";
+
+	ft_putstr(c);
+	return(0);
+}
+*/
