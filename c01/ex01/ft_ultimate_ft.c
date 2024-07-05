b7c20c5995e53a5b8ffe6ef75e68cf41e2c9ec11
majdsaadat <150346255+majdsaadat@users.noreply.github.com>
2024-07-05 19:18:08 +0300

    Create ft_ultimate_ft.c

---
+++ c01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** /
+/ /
+/ ::: :::::::: /
+/ ft_ultimate_ft.c :+: :+: :+: /
+/ +:+ +:+ +:+ /
+/ By: msaadat marvin@42.fr +#+ +:+ +#+ /
+/ +#+#+#+#+#+ +#+ /
+/ Created: 2024/06/12 09:59:53 by msaadat #+# #+# /
+/ Updated: 2024/06/12 10:10:16 by msaadat ### ########.fr /
+/ /
+/ ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void ft_ultimate_ft(int *********nbr)
+{
+********nbr = 42;
+}
+/*
+int main(void)
+{
+int a;
+
+a = 10;
+int	*ptr;
+int	**ptr1;
+int	***ptr2;
+int	****ptr3;
+int	*****ptr4;
+int	******ptr5;
+int	*******ptr6;
+int	********ptr7;
+int	*********ptr8;
+
+ptr = &a;
+ptr1 = &ptr;
+ptr2 = &ptr1;
+ptr3 = &ptr2;
+ptr4 = &ptr3;
+ptr5 = &ptr4;
+ptr6 = &ptr5;
+ptr7 = &ptr6;
+ptr8 = &ptr7;
+
+ft_ultimate_ft(ptr8);
+printf("%i",a);
+return (0);
+}*/
